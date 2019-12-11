89866550e02f72eeb730ddee727052c694fe2ef0
alfathony <email.alfathony@gmail.com>
2019-12-11 20:37:00 +0700

    terus coba

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,7 +9,7 @@ $route['membership/tiket-saya'] = 'Membership/tiketsaya';
 $route['membership/navbar_mb'] = 'Membership/membership';
 
 //$route['event/(:num)/(:any)'] = 'Event/detail/$1/$2';
-//$route['event/all-events'] = 'Event/all_event';
+$route['event/all-events'] = 'Event/all_event';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
